b5d9f2f9a70982607743af43e64fd146e9a43a76
David Francis <davidjayfrancis@gmail.com>
2019-12-18 18:34:15 -0800

    added some initial file structure

---
+++ src/components/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+ 
+const LandingPage = () => {
+    return (
+        <>
+            <h2>Your Properties</h2>
+            <p>Property info will go here</p>
+            {/* 
+                properties.map(property => {
+                    return <PropertyCard>
+                })
+                
+            */}
+            {// image of a big + } 
+            <div className='addProperty'>
+                
+            </div>
+        </>
+    )
+}
