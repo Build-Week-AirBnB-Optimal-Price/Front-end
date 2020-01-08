5fd8f68030a571d7aa97f0808b369eb086c831ec
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-01-07 20:26:27 -0500

    fixxed the header and created HomePageclea

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from "react-router-dom";
 
 //components
 import Header from "./components/Header.js";
+import HomePage from "./components/HomePage.js";
 import LandingPage from "./components/LandingPage.js";
 import About from "./components/About.js";
 import LoginForm from "./components/LoginForm";
@@ -14,7 +15,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Route exact path="/" component={LandingPage}></Route>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route path="/properties" component={LandingPage}></Route>
       <Route path="/about" component={About}></Route>
       <Route path="/login" component={LoginForm}></Route>
       <Route path="/register" component={Register}></Route>
